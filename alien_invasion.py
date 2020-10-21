5a0beca58ba424fb9d31fc7518fc4edc9f1822c7
aZade Dnpr <az_dnpr@aZades-MacBook-Air.local>
2020-10-21 19:51:34 +0200

    refactoring:_check_events & _update_screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 class AlienInvasion:
     """ Overall Class to manage game assets and behaviours"""
+
     def __init__(self):
         """ Initialize the game and set game resources"""
         #initialize the background setting
@@ -16,18 +17,27 @@ class AlienInvasion:
         # Set the current window caption
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+
     def run_game(self):
         """ Start the main loop for the game"""
         while True:
             #Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_event()
+            #Re-draw the screen during each pass through the loop
+            self._update_screen()
+                       
+    def _check_event(self):
+        """responds to key press and mouse events"""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                #Re-draw the screen during each pass through the loop
-                self.screen.fill(self.bg_color)
-                self.ship.blitme()
-                #Make the most recently drawn screen visible.
-                pygame.display.flip()
+
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 if __name__ == '__main__':
     #Make a game instance, and run the game
     ai = AlienInvasion()
