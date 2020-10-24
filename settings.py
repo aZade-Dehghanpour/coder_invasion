def2159c2f8f31e0d1ebaba4eeef01f87896f8ff
aZade Dnpr <azade.dehghanpour@gmail.com>
2020-10-24 15:00:22 +0200

    Set bullet seeting

--- settings.py
+++ settings.py
@@ -1,7 +1,14 @@
 class Settings:
     """ A class to store all settings for ALien Invasion"""
     def __init__(self):
+        #Screen setting
         self.width = 1200
         self.height = 800
         self.bg_color = (150,200,255)
+        #ship setting
         self.ship_speed = 2.5
+        #bullet setting
+        self.bullet_speed = 1
+        self.bullet_rect_hight = 10
+        self.bullet_rect_width = 5
+        self.bullet_color = (60,60,60)
